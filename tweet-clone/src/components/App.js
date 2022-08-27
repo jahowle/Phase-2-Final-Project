8199e34cf467da7f4703d10c5094d4f7758a0d3c
Jonny <jonny.howle@gmail.com>
2022-08-27 12:10:57 -0600

    Initialize app

---
+++ tweet-clone/src/components/App.js
@@ -0,0 +1,13 @@
+import '../App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Tweet Clone</h1>
+     <Sidebar />
+    </div>
+  );
+}
+
+export default App;
