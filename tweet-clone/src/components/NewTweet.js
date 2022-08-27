1cfab5ec53caf59b2c3b7017a9b6877506ca65fa
Jonny <jonny.howle@gmail.com>
2022-08-27 13:32:08 -0600

    added basic form components

--- tweet-clone/src/components/NewTweet.js
+++ tweet-clone/src/components/NewTweet.js
@@ -3,6 +3,11 @@ import React from "react";
 function NewTweet() {
     return(
         <div id="new-tweet">
+            <form>
+                <input id="tweet-input" type="text" placeholder="Say something...">   
+                </input>
+                <input id="post-btn"  type="submit" value="Post"></input>
+            </form>
         </div>
     )
 }
