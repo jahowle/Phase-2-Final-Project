40b3febe5688f28913d22a4db89bd2b5e0958283
Jonny <jonny.howle@gmail.com>
2022-08-30 18:05:52 -0600

    added styling to nav links

--- tweet-clone/src/components/Sidebar.js
+++ tweet-clone/src/components/Sidebar.js
@@ -4,15 +4,23 @@ import { NavLink } from "react-router-dom";
 function Sidebar() {
     return(
         <div className="sidebar">
+            <button className="nav-button">
             <NavLink to="/" exact>
                 Home
             </NavLink>
+            </button>
+           
+            <button className="nav-button"> 
             <NavLink to="/login" exact>
                 Login
             </NavLink>
+            </button>
+           
+           <button className="nav-button">
             <NavLink to="/profile" exact>
                 Profile
             </NavLink>
+            </button>
         </div>
     )
 }
