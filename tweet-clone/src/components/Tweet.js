fb0452edb4e7b497e7a790ecdd885399cbd2b4ea
Jonny <jonny.howle@gmail.com>
2022-09-01 08:33:10 -0600

    added static like component to tweets

--- tweet-clone/src/components/Tweet.js
+++ tweet-clone/src/components/Tweet.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { HiHeart, HiOutlineHeart } from "react-icons/hi"; 
 
 function Tweet({username, content,}) {
     return(
         <div className="tweet">
             <h3>{username}</h3>
             <p>{content}</p>
+            <div className="like-wrap"><HiOutlineHeart /> 0</div>
         </div>
     )
 }
