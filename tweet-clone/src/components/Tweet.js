b76af1998dd3f21b68e7eb5ba1b376a48a1661af
Jonny <jonny.howle@gmail.com>
2022-09-01 08:49:05 -0600

    like icon now updates on click

--- tweet-clone/src/components/Tweet.js
+++ tweet-clone/src/components/Tweet.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import { HiHeart, HiOutlineHeart } from "react-icons/hi"; 
 
 function Tweet({username, content,}) {
+
+    const [liked, setLiked] = useState(false)
+
+    function handleLike() {
+        setLiked(!liked)
+    }
+
     return(
         <div className="tweet">
             <h3>{username}</h3>
             <p>{content}</p>
-            <div className="like-wrap"><HiOutlineHeart /> 0</div>
+            <div className="like-wrap">{liked ? <HiOutlineHeart onClick={handleLike}/> : <HiHeart onClick={handleLike} />} {liked ? 0 : 1}</div>
         </div>
     )
 }
